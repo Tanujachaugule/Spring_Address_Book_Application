457dbd0047ca41d84cd2e0475b668fff9b46ff88
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-06-27 11:32:12 +0530

    Added: created rest controller using various http methods

---
+++ src/main/java/com/bridgelabz/addressbookapplication/controller/AddressBookController.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.addressbookapplication.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/addressbookapplication")
+public class AddressBookController {
+    @RequestMapping(value = {" ","/","/get"})
+    public ResponseEntity<String> getAddressBookData(){
+        return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
+    }
+@GetMapping("/get/{personId}")
+    public ResponseEntity<String> getAddressBookData(@PathVariable ("personId")int personId){
+        return new ResponseEntity<String>("Get Call Success for id: "+ personId, HttpStatus.OK);
+    }
+    @PostMapping("/addnewcontact")
+    public ResponseEntity<String> addNewContact(@RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<String>("Created New Contact for: "+ addressBookDTO, HttpStatus.OK);
+    }
+
+
+    @PutMapping("/updatecontact")
+    public ResponseEntity<String> updateContact(@RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<String>("Updated Contact : "+ addressBookDTO, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/delete/{empId}")
+    public ResponseEntity<String> deleteContact(@PathVariable("personId") int personId) {
+        return new ResponseEntity<String>("Delete Call Success for id: " +personId, HttpStatus.OK);
+    }
+}
